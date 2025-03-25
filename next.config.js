2ba5f00d55c39054e486756a002427df8c5457aa
Luciano Tulio <luciano.tulio@outlook.com>
2025-03-25 13:41:45 -0300

    Configura deploy na Vercel

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ['fonts.googleapis.com'],
   },
+  // Adiciona configurações para garantir que a aplicação funcione em produção
+  reactStrictMode: true,
+  swcMinify: true,
+  // Configura o diretório de build
+  distDir: '.next',
+  // Configura o diretório de arquivos estáticos
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://com-quem-eu-falo.vercel.app' : '',
 }
 
 module.exports = nextConfig 
